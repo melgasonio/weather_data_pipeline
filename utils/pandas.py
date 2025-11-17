7ef3fd69914885a24f3109f7a7f7c65eed020840
unknown <soniomelga@gmail.com>
2025-11-17 02:29:23 -0500

    refactor: delete unnecessary dataframe methods

--- utils/pandas.py
+++ utils/pandas.py
@@ -4,15 +4,3 @@ import numpy as np
 # Convert an array of dictionaries to a dataframe
 def convert_to_df(arr):
     return pd.DataFrame(arr)
-
-# Remove a set of columns from a dataframe
-def delete_col(df, columns_to_drop):
-    return df.drop(columns=columns_to_drop)
-
-# Add a column with a content to a dataframe
-def add_col(df, col, content):
-    try:
-        df[col] = content
-        return df
-    except ValueError:
-        raise ValueError(f"Content length mismatch on column {col}")
